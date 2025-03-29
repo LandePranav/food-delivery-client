1edaf98f416ebbf55d7574661ee89d27b24aac43
LandePranav <pranavlande1@gmail.com>
2025-03-29 16:02:13 +0530

    fix: export authoptions config

---
+++ src/app/api/auth/auth.config.ts
@@ -0,0 +1,54 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import GoogleProvider from "next-auth/providers/google";
+import { AuthOptions, DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      email: string;
+      name: string;
+      image?: string;
+    } & DefaultSession["user"]
+  }
+}
+
+const prisma = new PrismaClient();
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  session: { strategy: "jwt" as const },
+  callbacks: {
+    async session({ session, token }) {
+      if (token?.sub && session?.user) {
+        session.user.id = token.sub;
+        session.user.email = token.email || "";
+        session.user.name = token.name || "";
+        session.user.image = token.picture || undefined;
+      }
+      return session;
+    },
+    async signIn({ user }) {
+      if (!user.email) {
+        return false; // Prevent sign-in if email is not available
+      }
+      const existingSeller = await prisma.seller.findUnique({
+        where: { email: user.email },
+      });
+
+      if (existingSeller) {
+        return false; // Prevent OAuth login for sellers
+      }
+
+      return true;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET ?? "",
+}; 
